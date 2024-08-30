aeddd3f36f9956adfbae3a0cf6323b880b2e4f15
sourav-kumar4297 <souravkumar4297@gmail.com>
2024-08-30 14:21:56 +0530

    Adding parcel features

--- Day 2/App.js
+++ Day 2/App.js
@@ -1,5 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Parcel Features 
+//1. Created a Server
+//2. HMR - Hot Module Replacement
+//3. Caching while development
+//4. Bundling
+//5. Minify
+//6. File watcher alogorithm -- using C++(Execute files when they change)
+//7. Clearing the code (like console.log())
+//8. Dev and Production build mode
+//9. Super fast build mode
+//10. Image optimization
+//11. Compression
+//12. Compatable with the older version of browsers
+//13. HTTPS support on - Development mode also
+//14. Port Number Support -[if you are using port 1234 and run another app side by side then parcel 
+//    will take care of it and provide another port number to the other app as well]
+//15. Consistent hashing algorithm
+//16. Zero Config
+//17. Tree Shaking
+
+
+
+
+
 const reactroot = ReactDOM.createRoot(document.querySelector(".root"));
 const element = React.createElement(
   "h3",
